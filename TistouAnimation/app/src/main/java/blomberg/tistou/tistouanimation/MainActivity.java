5101125c69ef1f5c092cfab8805c5aa9baec8db7
Tistou <tistou.blomberg@gmail.com>
2016-04-19 14:15:15 +0200

    added 2 animations Imageview that animates Button that makes it so

--- TistouAnimation/app/src/main/java/blomberg/tistou/tistouanimation/MainActivity.java
+++ TistouAnimation/app/src/main/java/blomberg/tistou/tistouanimation/MainActivity.java
@@ -2,12 +2,48 @@ package blomberg.tistou.tistouanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.MAIN_IMG_ANDROID)ImageView imageViewAndroid;
+    @Bind(R.id.MAIN_BTN_SLIDEDOWN)Button btnSlideDown;
+
+    //Animations
+    Animation animSlidedown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setupAnimations();
+        setupButtons();
     }
+
+    private void setupAnimations() {
+        animSlidedown = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+    }
+
+    private void setupButtons() {
+        btnSlideDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slideDownAnimation();
+            }
+        });
+    }
+
+    private void slideDownAnimation() {
+        imageViewAndroid.startAnimation(animSlidedown);
+    }
+
 }
